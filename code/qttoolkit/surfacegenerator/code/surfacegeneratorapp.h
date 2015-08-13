393a90efd1b57c17b58312f19d2ec4521844be17
Gustav Sterbrant <gustav.sterbrant@gmail.com>
2015-08-13 13:51:38 +0200

    *qttoolkit* new: - Surface generator: converts models into surfaces and gathers up models with equal surfaces into one.

---
+++ code/qttoolkit/surfacegenerator/code/surfacegeneratorapp.h
@@ -0,0 +1,30 @@
+#pragma once
+//------------------------------------------------------------------------------
+/**
+	App entry point for surface generator app.
+	
+	(C) 2015 Individual contributors, see AUTHORS file
+*/
+//------------------------------------------------------------------------------
+#include "core/refcounted.h"
+#include "app/application.h"
+#include "surfacegeneratorwindow.h"
+
+namespace ToolkitUtil
+{
+class SurfaceGeneratorApp : public App::Application
+{
+public:
+	/// constructor
+	SurfaceGeneratorApp();
+	/// destructor
+	~SurfaceGeneratorApp();
+
+	/// opens the app
+	virtual bool Open();
+	/// close the app
+	virtual void Exit();
+private:
+	SurfaceGeneratorWindow window;
+};
+} // namespace ToolkitUtil
