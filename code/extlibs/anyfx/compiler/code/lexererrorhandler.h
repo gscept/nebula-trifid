eb3f9217294914605f1f6838c3217d2af6aa2e3c
Gustav Sterbrant <gustav.sterbrant@gmail.com>
2016-02-09 14:56:48 +0100

    *anyfx* - Dereferenced local anyfx version in favor of separately maintained version.

--- code/extlibs/anyfx/compiler/code/lexererrorhandler.h
+++
@@ -1,7 +0,0 @@
-#ifndef LEXERERRORHANDLER_H
-#define LEXERERRORHANDLER_H
-void LexerError(pANTLR3_BASE_RECOGNIZER recognizer, pANTLR3_UINT8* tokens);
-void PrintLexerException(pANTLR3_EXCEPTION ex, const char* file);
-#endif
-
-
