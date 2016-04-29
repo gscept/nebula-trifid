6f08c162f39726cd896258fcff70b28f8a6dc3e5
Gustav Sterbrant <gustav.sterbrant@ltu.se>
2016-04-29 13:33:35 +0200

    Reverted bad default camera settings.

--- code/render/graphics/cameraentity.cc
+++ code/render/graphics/cameraentity.cc
@@ -23,7 +23,7 @@ CameraEntity::CameraEntity()
 {
     this->SetType(GraphicsEntityType::Camera);
     float aspectRatio = DisplayDevice::Instance()->GetDisplayMode().GetAspectRatio();
-    this->camSettings.SetupPerspectiveFov(n_deg2rad(90.0f), aspectRatio, 0.0001f, 2500.0f);
+    this->camSettings.SetupPerspectiveFov(n_deg2rad(60.0f), aspectRatio, 0.01f, 2500.0f);
 }
 
 //------------------------------------------------------------------------------
